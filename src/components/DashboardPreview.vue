b20dce9a5f12e24917aabcc341a4fd227bd28c3d
anibalalpizar <anibal.alpizar14@gmail.com>
2025-03-07 21:27:56 -0600

    add DashboardPreview component for displaying a visual representation of the dashboard

---
+++ src/components/DashboardPreview.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container mx-auto py-20">
+    <div class="max-w-5xl mx-auto px-4 bg-[#FDF5DF]">
+      <img
+        src="/images/dashboard-preview.png"
+        alt="Dashboard Preview"
+        class="w-full rounded-3xl shadow-xl"
+      />
+    </div>
+  </section>
+</template>
